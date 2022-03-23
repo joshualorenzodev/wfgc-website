058ce995193f7c2009e44858235cae001e089e11
Joshua Lorenzo <joshualorenzo.dev@gmail.com>
2022-03-23 16:20:25 -0500

    Creates general content of landing page

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+
+
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./assets/favicon/site.webmanifest">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap" rel="stylesheet">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/style.css">
+
+    <title>Winnipeg Foursquare Gospel Church</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <header class="main-header">
+            <div class="logo">
+                <img src="./assets/logo_transparent.png" alt="Logo" id="wfgc-logo">
+            </div>
+            <nav class="main-nav">
+                <ul class="nav-bar">
+                    <li class="nav-item"><a href="#about" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="#ministries" class="nav-link">Ministries</a></li>
+                    <li class="nav-item"><a href="#sermon" class="nav-link">Sermon</a></li>
+                    <li class="nav-item"><a href="#give" class="nav-link">Give</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="hero">
+            <h1 class="hero-text">Jesus Christ is the same yesterday and today and forever.</h1>
+            <p class="tagline">Join us Sunday at 10:00am In-Person & Online</p>
+            <div class="hero-btns">
+                <a href="https://www.facebook.com/bethel.churchwinnipeg/" class="livestream-btn">Livestream</a>
+                <a href="https://www.youtube.com/channel/UCj19-mZHRsHR6l6Ncyl3elg" class="ytb-btn">YouTube</a>
+            </div>
+        </div>
+
+        <section class="cta-cards">
+            <div class="cta-card">
+                <h2 class="card-title">Stay in the loop</h2>
+                <p class="card-text">Get a run down of what's on this week at Gateway</p>
+                <a href="#" class="card-btn">This Week</a>
+            </div>
+            <div class="cta-card">
+                <h2 class="card-title">Join a group</h2>
+                <p class="card-text">Connecting outside of Sunday is a great way to build relationships and your faith.</p>
+                <a href="#" class="card-btn">More Info</a>
+            </div>
+            <div class="cta-card">
+                <h2 class="card-title">Reach out</h2>
+                <p class="card-text">We'd be honoured to get to know you. Send us a message today.</p>
+                <a href="#" class="card-btn">Connect</a>
+            </div>
+        </section>
+
+        <section id="prayer">
+            <h2>Need a prayer?</h2>
+            <p>Life happens. In moments of difficulty or uncertainty prayer is a powerful response because we trust our situation to the creator of all things. We'd love to pray for you.</p>
+            <a href="#" class="prayer-btn">Ask for Prayer</a>
+        </section>
+
+        <section class="cta-content">
+            <div class="content-card">
+                <a href="#">
+                    <img class="content-icon" src="./assets/mail.png" alt="Newsletter">
+                    <h2 class="content-title">Weekly Email</h2>
+                </a>
+                <p class="content-text">Subscribe to our weekly email newsletter</p>
+            </div>
+            <div class="content-card">
+                <a href="#">
+                    <img class="content-icon" src="./assets/watch.png" alt="Watch">
+                    <h2 class="content-title">Watch</h2>
+                </a>
+                <p class="content-text">Check out recent sermonds from WFGC</p>
+            </div>
+            <div class="content-card">
+                <a href="#">
+                    <img class="content-icon" src="./assets/give.png" alt="Give">
+                    <h2 class="content-title">Give</h2>
+                </a>
+                <p class="content-text">Bring your tithes and offerings online</p>
+            </div>
+        </section>
+
+        <footer class="main-footer">
+            <div class="footer-item">
+                <h4 class="footer-title">ADDRESS</h4>
+                <p>898 Henderson Hwy</p>
+                <p>Winnipeg, Manitoba</p>
+                <p>R2K 3T4</p>
+                <p>204.898.9197</p>
+            </div>
+
+            <div class="footer-item">
+                <h4 class="footer-title">SCHEDULE</h4>
+                <p>Sunday School 10:00am - 10:30am</p>
+                <p>Prayer Worship 10:30am - 11:00am</p>
+                <p>Sunday Service 11:00am - 12:30pm</p>
+            </div>
+
+            <div class="footer-item">
+                <div class="socials">
+                    <img class="footer-icon" src="./assets/facebook.png" alt="Facebook">
+                    <img class="footer-icon" src="./assets/youtube.png" alt="YouTube">
+                    <img class="footer-icon" src="./assets/instagram.png" alt="Instagram">
+                </div>
+            </div>
+        </footer>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
