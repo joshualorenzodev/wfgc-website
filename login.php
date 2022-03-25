1ed27819596bb1405ccb5087d4391b92e2774b41
Joshua Lorenzo <joshualorenzo.dev@gmail.com>
2022-03-24 21:29:38 -0500

    Updates login page UI and adds registration page

--- login.php
+++ login.php
@@ -9,29 +9,32 @@
         $err_msg = "Login failed. Try again!";
     }
 ?>
-
-<?php include './src/inc/header.php'?>
-<div class="container-fluid">
-    <!-- <?php include './src/inc/nav.php'?>  -->
-    <div class="container">
-        <div class="col-sm-3">
-            <h4 class="text-center">Login</h4>
-            <p class="error_message"><?=$err_msg?></p>
-            <form action="./src/libs/authenticate.php" method="post">
-                <div class="form-group">
-                    <input type="text" name="username" id="username" class="form-control" placeholder="Username"> 
-                </div>
-                
-                <div class="input-group">
-                    <input type="password"   name="password" id="password" class="form-control" placeholder="Password"> 
-                    <span class="input-group-btn">
-                        <button class="btn btn-primary" name="login" type="submit">Submit</button>
-                    </span>
-                </div>
-            </form>
-            <p class="text-center">Not a member? <a href="register.php">Register here</a></p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/register.css">
+    <title>Login</title>
+</head>
+<body>
+<main>
+    <form action="./src/libs/authenticate.php" method="post">
+        <h1>Login</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
         </div>
-    </div>
-    <!-- <?php include './src/inc/footer-section.php'?> -->
-</div>
-<?php include './src/inc/footer.php'?>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+
+        <p class="error_message"><?=$err_msg?></p>
+
+        <button type="submit" name="login">Login</button>
+        <footer>Not a member? <a href="register.php">Register here</a></footer>
+    </form>
+</main>
+</body>
+</html>
